125d7c41ec31e76e9f83868457aeee1758fcf4ce
roee <roee@starkware.co>
2025-10-21 14:35:22 +0300

    feat: add basic devnet test

--- devnet_tests/system_test.py
+++ devnet_tests/system_test.py
@@ -3,5 +3,9 @@ from test_utils.starknet_test_utils import StarknetTestUtils
 
 
 # TODO: Implement system tests for the forked Starknet environment.
-def test_dummy(starknet_forked_with_impersonated_accounts: StarknetTestUtils):
+def test_dummy(
+    starknet_forked_with_impersonated_accounts: StarknetTestUtils,
+    declare_contract,
+    declare_perpetuals_core_contract: int,
+):
     assert starknet_forked_with_impersonated_accounts
