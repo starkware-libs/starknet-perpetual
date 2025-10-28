418d87d6feb2863be4db0477d31fc94289580b69
Omri Ben Ary <omri.b@starkware.co>
2025-10-28 17:43:04 +0200

    test(perps): add python infra

--- devnet_tests/conftest.py
+++ devnet_tests/conftest.py
@@ -1,6 +1,25 @@
 import pytest
 from typing import Iterator, Callable
+import pytest_asyncio
+from starknet_py.net.schemas.common import Uint64
 from test_utils.starknet_test_utils import StarknetTestUtils
+from starknet_py.net.models.chains import StarknetChainId
+from test_utils.starknet_test_utils import KeyPair
+
+from starknet_py.net.account.account import Account
+from starknet_py.net.models.address import Address
+from starknet_py.net.client_models import Call
+from starknet_py.contract import Contract
+from starknet_py.proxy.contract_abi_resolver import ContractAbiResolver, ProxyConfig
+from test_utils.starknet_test_utils import load_contract
+from scripts.script_utils import get_project_root
+from pathlib import Path
+import os
+from starknet_py.net.client_models import ResourceBoundsMapping, ResourceBounds
+
+
+perpetuals_Core = "perpetuals_Core"
+vault_contract = "vault_ProtocolVault"
 
 
 @pytest.fixture(scope="session")
@@ -27,18 +46,20 @@ def deployer_address() -> int:
     return 0x0522E5BA327BFBD85138B29BDE060A5340A460706B00AE2E10E6D2A16FBF8C57
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def starknet_forked(
     starknet_test_utils_factory: Callable[..., Iterator[StarknetTestUtils]]
 ) -> Iterator[StarknetTestUtils]:
     with starknet_test_utils_factory(
         fork_network="https://rpc.starknet.lava.build/",
         fork_block=1844544,
+        starknet_chain_id=StarknetChainId.MAINNET,
+        request_body_size_limit=20_000_000,
     ) as val:
         yield val
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def starknet_forked_with_impersonated_accounts(
     starknet_forked: StarknetTestUtils, operator_address: int, deployer_address: int
 ) -> StarknetTestUtils:
@@ -47,6 +68,130 @@ def starknet_forked_with_impersonated_accounts(
     """
     client = starknet_forked.starknet.get_client()
     for address in [operator_address, deployer_address]:
-        client.impersonate_account(address)
+        client.impersonate_account_sync(address)
 
     return starknet_forked
+
+
+@pytest.fixture(scope="function")
+def operator_account(
+    starknet_forked_with_impersonated_accounts: StarknetTestUtils,
+    operator_address: int,
+) -> Account:
+    """
+    Return an Account instance for the impersonated operator account.
+    """
+    client = starknet_forked_with_impersonated_accounts.starknet.get_client()
+    operator_account = Account(
+        client=client,
+        address=Address(operator_address),
+        # Use a dummy private key since the account is impersonated.
+        key_pair=KeyPair.from_private_key(1),
+        chain=StarknetChainId.MAINNET,
+    )
+    return operator_account
+
+
+@pytest.fixture(scope="function")
+def deployer_account(
+    starknet_forked_with_impersonated_accounts: StarknetTestUtils,
+    deployer_address: int,
+) -> Account:
+    """
+    Return an Account instance for the impersonated deployer account.
+    """
+    client = starknet_forked_with_impersonated_accounts.starknet.get_client()
+    deployer_account = Account(
+        client=client,
+        address=Address(deployer_address),
+        # Use a dummy private key since the account is impersonated.
+        key_pair=KeyPair.from_private_key(1),
+        chain=StarknetChainId.MAINNET,
+    )
+    return deployer_account
+
+
+@pytest_asyncio.fixture(scope="function")
+async def declare_perpetuals_core_contract(
+    starknet_forked_with_impersonated_accounts: StarknetTestUtils,
+    deployer_account: Account,
+    contract_address: int,
+) -> int:
+    compiled_contract_casm = load_contract(
+        contract_name=f"{perpetuals_Core}.compiled_contract_class",
+        base_path=Path(os.path.join(get_project_root(), "target", "dev")),
+    )
+    compiled_contract = load_contract(
+        contract_name=f"{perpetuals_Core}.contract_class",
+        base_path=Path(os.path.join(get_project_root(), "target", "dev")),
+    )
+
+    declare_result = await Contract.declare_v3(
+        account=starknet_forked_with_impersonated_accounts.starknet.accounts[0],
+        compiled_contract=compiled_contract,
+        compiled_contract_casm=compiled_contract_casm,
+        auto_estimate=False,
+        resource_bounds=ResourceBoundsMapping(
+            l1_gas=ResourceBounds(max_amount=10**15, max_price_per_unit=10**12),
+            l1_data_gas=ResourceBounds(max_amount=10**15, max_price_per_unit=10**12),
+            l2_gas=ResourceBounds(max_amount=10**15, max_price_per_unit=10**12),
+        ),
+    )
+    await declare_result.wait_for_acceptance(check_interval=0.1)
+    return declare_result.class_hash
+
+
+@pytest_asyncio.fixture(scope="function")
+async def upgrade_perpetuals_core_contract(
+    declare_perpetuals_core_contract: int,
+    contract_address: int,
+    deployer_account: Account,
+    operator_account: Account,
+) -> Contract:
+    abi, cairo_version = await ContractAbiResolver(
+        address=contract_address,
+        client=deployer_account.client,
+        proxy_config=ProxyConfig(),
+    ).resolve()
+
+    deployer_contract = Contract(
+        address=contract_address,
+        abi=abi,
+        provider=deployer_account,
+        cairo_version=cairo_version,
+    )
+
+    invocation = await deployer_contract.functions["add_new_implementation"].invoke_v3(
+        {
+            "impl_hash": declare_perpetuals_core_contract,
+            "eic_data": None,
+            "final": False,
+        },
+        auto_estimate=True,
+    )
+    await invocation.wait_for_acceptance(check_interval=0.1)
+
+    invocation = await deployer_contract.functions["replace_to"].invoke_v3(
+        {
+            "impl_hash": declare_perpetuals_core_contract,
+            "eic_data": None,
+            "final": False,
+        },
+        auto_estimate=True,
+    )
+    await invocation.wait_for_acceptance(check_interval=0.1)
+
+    abi, cairo_version = await ContractAbiResolver(
+        address=contract_address,
+        client=operator_account.client,
+        proxy_config=ProxyConfig(),
+    ).resolve()
+
+    operator_contract = Contract(
+        address=contract_address,
+        abi=abi,
+        provider=operator_account,
+        cairo_version=cairo_version,
+    )
+
+    return operator_contract
--- devnet_tests/system_tests.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from test_utils.starknet_test_utils import StarknetTestUtils
-
-
-# TODO: Implement system tests for the forked Starknet environment.
-def test_dummy(starknet_forked_with_impersonated_accounts: StarknetTestUtils):
-    assert starknet_forked_with_impersonated_accounts
