ecb320e0cdc80b937caef074c8a7c132de5e9c65
Mohammad Nassar <mohammad@starkware.co>
2025-12-21 18:18:21 +0200

    feat(perpetuals): support multi collateral in withdraw

--- devnet_tests/perpetuals_test_utils.py
+++ devnet_tests/perpetuals_test_utils.py
@@ -427,6 +427,7 @@ class PerpetualsTestUtils:
             .functions["withdraw_request"]
             .invoke_v3(
                 signature,
+                formatted_asset_id(collateral_asset_id),
                 self.get_account_address(account),
                 formatted_position_id(self.get_account_position_id(account)),
                 amount,
@@ -441,6 +442,7 @@ class PerpetualsTestUtils:
         async def _process_withdraw(account: Account, amount: int, expiration: int, salt: int):
             invocation = await self.operator_contract.functions["withdraw"].invoke_v3(
                 await self.consume_operator_nonce(),
+                formatted_asset_id(await self.get_collateral_asset_id()),
                 self.get_account_address(account),
                 formatted_position_id(self.get_account_position_id(account)),
                 amount,
